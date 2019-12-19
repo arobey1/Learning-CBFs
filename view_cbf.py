ee1ebe369f08a2c215c8a23ab63b2c0e62703d52
Alex Robey <arobey1@seas.upenn.edu>
2019-12-18 21:20:44 -0800

    added option in 3dplot function to view both CBFs side by side or composed depending on user supplied flag

--- view_cbf.py
+++ view_cbf.py
@@ -1,4 +1,6 @@
-import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use("TKAgg")
+from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 from train_net import trial
@@ -11,47 +13,86 @@ from SeqNet import Net
 from MultiLayerNet import Network
 import os
 
+INPUT_DIM = 6
+OUTPUT_DIM = 1
+NET_DIMS = [INPUT_DIM, 100, 50, OUTPUT_DIM]
+LR = 1e-3
+N_EPOCHS = 1000
+LOSS = torch.nn.MSELoss
+SAVE_NET = True
+OPTIMIZER = torch.optim.Adam
+
 def main():
 
     states, values = get_states_and_values()
-    net, errors = trial()
+    net, train_err, test_err = trial(NET_DIMS, lr=LR, loss=LOSS, n_epochs=N_EPOCHS, save_net=SAVE_NET, opt=OPTIMIZER)
+    # net = torch.load(os.path.join(os.getcwd(), 'saved_models/model.pt'))
     data = Variable(torch.tensor(states).float())
     predicted_values = net(data).detach().numpy()
+    plot3d(states, values, predicted_values, together=True)
+    # plot_train_errors(train_err, test_err)
 
-    # matlab_arr = savemat('data/data_for_haimin.m', {
-    #     'X': states,
-    #     'y_pred': predicted_values,
-    # })
-
-    plot3d(states, values, predicted_values)
-
-
-    data_pt = states[0,:]
-    # print(states[0,:])
-    #
-    calc_grad(net, data_pt)
-    #
-    # new_net = torch.load(os.path.join(os.getcwd(), 'saved_models/model.pt'))
-    # new_net.eval()
-    # calc_grad(new_net, data_pt)
+    savemat(os.path.join(os.getcwd(), 'data/data_for_haimin'), mdict = {
+        'states': states,
+        'true_values': values,
+        'learned_values': predicted_values
+    })
 
 
+def plot3d(states:np.ndarray, values:np.ndarray, predicted_values: np.ndarray, together=False) -> None:
+    """Creates a 3D scatter plot of states and values for learned and true CBF
 
-def plot3d(states, values, predicted_values):
+    Args:
+        states: states for CBF
+        values: values of true CBF h(x)
+        predicted_values: values from learned function h(x)
+        together: plots true and predicted values on same plot if true
+
+    """
+
     rel_x_dist = states[:,0] - states[:,3]
     rel_y_dist = states[:,1] - states[:,4]
-
     fig = plt.figure()
-    ax = fig.gca(projection='3d')
-    ax.scatter3D(rel_x_dist, rel_y_dist, predicted_values, c=predicted_values, cmap='Blues')
-    ax.scatter3D(rel_x_dist, rel_y_dist, values, c=values, cmap='Reds', alpha=0.2)
+
+    if together is True:
+        ax = fig.gca(projection='3d')
+        ax.scatter3D(rel_x_dist, rel_y_dist, predicted_values, c=predicted_values, cmap='Reds', label="Learned CBF")
+        ax.scatter3D(rel_x_dist, rel_y_dist, values, c=values, cmap='Blues', alpha=0.2, label='True CBF')
+        ax.set_title('True and Learned CBFs')
+        ax.set_xlabel('Relative x distance')
+        ax.set_ylabel('Relative y distance')
+        ax.legend()
+    else:
+        l_ax = fig.add_subplot(1, 2, 1, projection='3d')
+        l_ax.scatter3D(rel_x_dist, rel_y_dist, values, c=predicted_values, cmap='Blues')
+        l_ax.set_title('True CBF')
+        l_ax.set_xlabel('Relative x distance')
+        l_ax.set_ylabel('Relative y distance')
+        r_ax = fig.add_subplot(1, 2, 2, projection='3d')
+        r_ax.scatter3D(rel_x_dist, rel_y_dist, predicted_values, c=values, cmap='Reds')
+        r_ax.set_title('Learned CBF')
+        r_ax.set_xlabel('Relative x distance')
+        r_ax.set_ylabel('Relative y distance')
 
     plt.show()
 
 
-def plot_train_errors(errors):
-    plt.plot(range(len(errors)), errors, label='Training error')
+def plot_train_errors(train_err, test_err):
+    """Short summary.
+
+    Args:
+        train_err: list of training errors at each epoch
+        test_err: list of test errors at each epoch
+    """
+
+    plt.plot(range(len(train_err)), train_err, 'r', label='Training error')
+    plt.plot(range(len(test_err)), test_err, 'b', label='Test error')
     plt.legend()
+    plt.grid()
+    plt.xlabel('Epoch number')
+    plt.ylabel('MSE')
+    plt.title('Training and Test Error for Learned CBF')
+    plt.show()
 
 def calc_grad(net, x):
 
