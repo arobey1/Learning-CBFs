c2472cfbf88fc929ed2963571cba252eefd0c0b9
Alex Robey <arobey1@seas.upenn.edu>
2019-12-18 21:18:35 -0800

    changed order of input arguments

--- SeqNet.py
+++ SeqNet.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class Net(nn.Module):
-    def __init__(self, num_hidden, input_dim, output_dim):
+    def __init__(self, input_dim, num_hidden, output_dim):
         super(Net, self).__init__()
         self.fc1 = nn.Linear(input_dim, num_hidden)
         self.fc2 = nn.Linear(num_hidden, output_dim)
